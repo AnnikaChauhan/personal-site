1d8c177aff969e5fedc4d761ec368f55bb2e8da0
Annika Chauhan <annikachauhan@annikas-mbp.home>
2020-01-26 12:39:10 +0000

    260120 12.39pm

--- script.js
+++ script.js
@@ -15,39 +15,39 @@ prjO.onclick = function() {
     //console.log('hello');
     prjO.style.display = 'none';
     prjX.style.display = 'initial';
-    document.getElementById('mrPrj').style.display = 'initial';
+    document.getElementById('aPrj').style.display = 'initial';
 }
 
 prjX.onclick = function() {
     prjO.style.display = 'initial';
     prjX.style.display = 'none';
-    document.getElementById('mrPrj').style.display = 'none';
+    document.getElementById('aPrj').style.display = 'none';
 }
 
 abtO.onclick = function() {
     //console.log('hello');
     abtO.style.display = 'none';
     abtX.style.display = 'initial';
-    document.getElementById('mrAbt').style.display = 'initial';
+    document.getElementById('aAbt').style.display = 'initial';
 }
 
 abtX.onclick = function() {
     abtO.style.display = 'initial';
     abtX.style.display = 'none';
-    document.getElementById('mrAbt').style.display = 'none';
+    document.getElementById('aAbt').style.display = 'none';
 }
 
 cntctO.onclick = function() {
     //console.log('hello');
     cntctO.style.display = 'none';
     cntctX.style.display = 'initial';
-    document.getElementById('mrCntct').style.display = 'initial';
+    document.getElementById('aCntct').style.display = 'initial';
 }
 
 cntctX.onclick = function() {
     cntctO.style.display = 'initial';
     cntctX.style.display = 'none';
-    document.getElementById('mrCntct').style.display = 'none';
+    document.getElementById('aCntct').style.display = 'none';
 }
 
 // style sheet swap
