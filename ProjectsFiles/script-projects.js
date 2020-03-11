8ccfc64de102c82d17f4afd26ff4cff1c7334784
Annika Chauhan <annikachauhan@annikas-mbp.home>
2020-03-11 21:33:34 +0000

    classes properly arranged and linking through properly to projects page, more projects to be added and maybe delete the form at the bottom

--- ProjectsFiles/script-projects.js
+++ ProjectsFiles/script-projects.js
@@ -1,18 +1,16 @@
 import projectData from "./data.js";
-
 import Project from "./Project.js";
 
 let projects = projectData.map((project) => {
     return new Project(
         project.name,
-        project.id,
-        project.importance,
+        project.weekProduced,
         project.description,
+        project.languages,
         project.launchLink,
-        project.codeLink
+        project.codeLink,
+        project.size
     );
 });
 
-console.log(projects);
-
-//projects.forEach((project) => $("#projectDatabase").append(project.html));
+projects.forEach((project) => $("#projectDatabase").append(project.html));
