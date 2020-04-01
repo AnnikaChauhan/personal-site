3c9842a71bac8789f359ee9661915dd2c0ad1593
Annika Chauhan <annikachauhan@annikas-mbp.home>
2020-04-01 22:12:36 +0100

    completed descriptions for all projects

--- ProjectsFiles/data.js
+++ ProjectsFiles/data.js
@@ -2,8 +2,8 @@ const projects = [
     {
         name: "NASA APOD & Mars Weather",
         weekProduced: 10,
-        description: "",
-        languages: ["HTML", " CSS", " JavaScript"," API"],
+        description: "A very small project, built very simply using HTML, CSS and JavaScript to practice using various API fetch methods. API fetches APOD and Mars Weather Data. Mars weather data was presented in a very interesting way and therefore getting the app to be fully dynamic was a fun challenge, the code is more interesting than the app!",
+        languages: ["HTML", " CSS", " JavaScript"," APIs"],
         launchLink: "https://annikachauhan.github.io/nasa-info/",
         codeLink: "https://github.com/AnnikaChauhan/nasa-info",
         size: "regular",
@@ -12,8 +12,8 @@ const projects = [
     {
         name: "Personal Project: Explorer",
         weekProduced: 9,
-        description: "",
-        languages: ["HTML", " CSS", " JavaScript"," React"," Firebase"," Email/Password Authentication", " APIs"],
+        description: "A personal interest project, built entirely alone. A React App where individuals can login to their account (or signup) using Firebase, read blogs by users about their travels (get from firestore), post short blogs about their own travels (post to firestore) and search cities to find the weather and local attractions in that city (two seperate APIs). A work in progress with many new features in the pipeline, access to Trello board is available on the GitHub repo.",
+        languages: ["HTML", " CSS", " JavaScript", " OOP", " React"," Firebase", " Getting from Firestore", " Posting to Firestore"," Email/Password Authentication", " Several APIs"],
         launchLink: "https://travel-project-d6827.firebaseapp.com/home",
         codeLink: "https://github.com/AnnikaChauhan/travel-project",
         size: "regular",
@@ -22,8 +22,8 @@ const projects = [
     {
         name: "Client Project: The Drop",
         weekProduced: 9,
-        description: "",
-        languages: ["HTML", " CSS", " JavaScript"," React"," Firebase"," Google Authentication"],
+        description: "The Drop was the client project of our cohort at _nology. The React app is a platform for artists to publish announcements about when they will next be releasing music to the public (using a form user interface for the artist to submit for each release) and fans can login to their side of the app to follow artists and see their releases. Tickets I worked on included: user authentication and Spotify API set up to access artist's that a fan follows on Spotify.",
+        languages: ["HTML", " CSS", " JavaScript", " OOP", " React"," Firebase"," Getting from Firestore", " Posting to Firestore"," Google & Email Authentication", " Spotify API"],
         launchLink: "https://the-drop-x-nology.web.app/",
         codeLink: "https://github.com/nology-tech/the-drop-x-nology",
         size: "regular",
@@ -32,8 +32,8 @@ const projects = [
     {
         name: "User Authentication",
         weekProduced: 8,
-        description: "",
-        languages: ["HTML", " CSS", " JavaScript"," React"," Firebase"," Google Authentication"],
+        description: "A very basic looking React App that tests the principles of private routing. When you click the sign in button you are redircted to a google login prompt, once you login you are redirected to the Private page, which can only be accessed once logged in. Once here, the anonymous information is replaced with your own information from Google.",
+        languages: ["HTML", " CSS", " JavaScript", " OOP", " React", " Private Routing", " Firebase"," Google Authentication"],
         launchLink: "https://users-authentication-e5d61.firebaseapp.com/public",
         codeLink: "https://github.com/AnnikaChauhan/users-authentication",
         size: "regular",
@@ -42,8 +42,8 @@ const projects = [
     {
         name: "Pokemon Database",
         weekProduced: 7,
-        description: "",
-        languages: ["HTML", " CSS", " JavaScript"," React"," Firebase"," Search Functionality"],
+        description: "A React App that has a collection of twelve pokemons collected from the Pokemon site, with some information about each of the Pokemon. The information is stored in the Firestore (Firebase database) and renders all pokemon in the database to the app. There is also advanced search functionality so that you can search Pokemon bu their name, category and weaknesses.",
+        languages: ["HTML", " CSS", " JavaScript", " OOP"," React"," Firebase", " Getting from Firestore"," Search Functionality"],
         launchLink: "https://pokemon-52216.firebaseapp.com/",
         codeLink: "https://github.com/AnnikaChauhan/pokeman-react-firebase",
         size: "regular",
@@ -52,8 +52,8 @@ const projects = [
     {
         name: "BookWorm",
         weekProduced: 6,
-        description: "A React App, built with a Firebase noSQL database (and Firebase storage) where books can be created. The firestore has been linked to the React app and all books from the database render to the app.",
-        languages: ["HTML", " CSS", " JavaScript"," React"," Firebase"],
+        description: "A React App, built with a Firebase noSQL database (and Firebase storage) where books can be created. The firestore has been linked to the React app and all books from the database render to the app. Additionally, in the navbar there is a count of the total number of books in the app and a list of authors of the books available.",
+        languages: ["HTML", " CSS", " JavaScript", " OOP"," React"," Firebase", " Getting from Firestore"],
         launchLink: "https://bookworm-d93e4.firebaseapp.com",
         codeLink: "https://github.com/AnnikaChauhan/bookworm-react-firebase",
         size: "regular",
@@ -70,7 +70,7 @@ const projects = [
         picture: "https://firebasestorage.googleapis.com/v0/b/annika-stuff.appspot.com/o/PersonalProjects%2Fguess_the_state.png?alt=media&token=c354ebd1-dc7e-46b1-950d-015c95688b02"
     },
     {
-        name: "Flappy Dragon (in progress)",
+        name: "Flappy Dragon (ish)",
         weekProduced: 5,
         description: "A game built in an attempt to recreate flappy bird, this was the first attempt at building an entire game from scratch. It is currently completing the bare principle of the game - the dragon falls but rises on a spacebar click and an obstacle moves across the screen, if they collide the game ends.",
         languages: ["HTML", " CSS", " JavaScript"],
@@ -83,7 +83,7 @@ const projects = [
         name: "This Site!",
         weekProduced: 4,
         description: "This site is something I spent time building from scratch, particularly my project page which has been built using classes and pulls data from a JavaScript file.",
-        languages: ["HTML", " CSS", " JavaScript"," jQuery", " Object Oriented Programming (Classes)"],
+        languages: ["HTML", " CSS", " JavaScript"," jQuery", " OOP"],
         launchLink: "www.annikachauhan.com",
         codeLink: "https://github.com/AnnikaChauhan/personal-site",
         size: "regular",
