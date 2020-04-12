3905ac67d9993eb6e2dcdcccd53fe6b0c23524e0
Annika Chauhan <annikachauhan@annikas-mbp.home>
2020-04-12 16:09:05 +0100

    slight amendments to data

--- ProjectsFiles/data.js
+++ ProjectsFiles/data.js
@@ -22,7 +22,7 @@ const projects = [
     {
         name: "Personal Project: Explorer",
         weekProduced: 9,
-        description: "A personal interest project, built entirely alone. A React App where individuals can login to their account (or signup) using Firebase, read blogs by users (get from firestore), post short blogs about their own travels (post to firestore) and search cities to find the weather and local attractions in that city (two APIs). Access to Trello board for feature list is available on the GitHub, additionally a list of tested components.",
+        description: "A personal interest project, built entirely alone. A React App where individuals can login to their account (or signup) using Firebase auth, read blogs by users (get from firestore), post short blogs about their own travels (post to firestore) and search cities to find weather and local attractions in that city (two APIs). Access to Trello board for feature list is available on the GitHub, additionally a list of tested components.",
         languages: ["HTML", " CSS", " JavaScript", " OOP", " React"," Firebase", " Getting from Firestore", " Posting to Firestore"," Email/Password Authentication", " Several APIs", " Testing"],
         launchLink: "https://travel-project-d6827.firebaseapp.com/home",
         codeLink: "https://github.com/AnnikaChauhan/travel-project",
@@ -42,7 +42,7 @@ const projects = [
     {
         name: "User Authentication",
         weekProduced: 8,
-        description: "A very basic looking React App that tests the principles of private routing and authentication. When you click the sign in button you are redircted to a google login prompt, once you login you are redirected to the Private page, which can only be accessed once logged in. Once here, the anonymous information is replaced with your own information from Google.",
+        description: "A very basic looking React App that tests the principles of private routing and authentication. When you click the sign in button you are redircted to a Google login prompt, once you login you are redirected to the Private page, which can only be accessed once logged in. Once here, the anonymous information is replaced with your own information from Google.",
         languages: ["HTML", " CSS", " JavaScript", " OOP", " React", " Private Routing", " Firebase"," Google Authentication"],
         launchLink: "https://users-authentication-e5d61.firebaseapp.com/public",
         codeLink: "https://github.com/AnnikaChauhan/users-authentication",
@@ -52,7 +52,7 @@ const projects = [
     {
         name: "Pokemon Database",
         weekProduced: 7,
-        description: "A React App that has a collection of twelve pokemons collected from the Pokemon site, with some information about each of the Pokemon. The information is stored in the Firestore (Firebase database) and renders all pokemon in the database to the app. There is also advanced search functionality so that you can search Pokemon bu their name, category and weaknesses.",
+        description: "A React App that has a collection of twelve pokemons collected from the Pokemon site, with some information about each of the Pokemon. The information is stored in the Firestore (Firebase database) and renders all pokemon in the database to the app. There is also advanced search functionality so that you can search Pokemon by their name, category and weaknesses.",
         languages: ["HTML", " CSS", " JavaScript", " OOP"," React"," Firebase", " Getting from Firestore"," Search Functionality"],
         launchLink: "https://pokemon-52216.firebaseapp.com/",
         codeLink: "https://github.com/AnnikaChauhan/pokeman-react-firebase",
